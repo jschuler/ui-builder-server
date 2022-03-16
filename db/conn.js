76543b9a6056011b0a3ccd718e2c38c744fbccac
Joachim Schuler <jdh.schuler@gmail.com>
2022-03-16 13:50:19 -0400

    cred

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,8 @@
 const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
+// const Db = process.env.ATLAS_URI;
+const USER = process.env.USER;
+const PW = process.env.PW;
+const Db = `mongodb+srv://${USER}:${PW}@uibuilder.6rqvr.mongodb.net/employees?retryWrites=true&w=majority`;
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
