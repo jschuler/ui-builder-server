072cc5345d3d171f460d6cf85322d0fcf32071c2
Joachim Schuler <jdh.schuler@gmail.com>
2022-03-16 14:03:38 -0400

    db

--- db/conn.js
+++ db/conn.js
@@ -3,6 +3,9 @@ const { MongoClient } = require("mongodb");
 const USER = process.env.MONGO_USER;
 const PW = process.env.MONGO_PW;
 const Db = `mongodb+srv://${USER}:${PW}@uibuilder.6rqvr.mongodb.net/employees?retryWrites=true&w=majority`;
+
+console.log(Db);
+
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
